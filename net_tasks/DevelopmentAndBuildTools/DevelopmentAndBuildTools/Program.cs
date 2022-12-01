033e8eeeecb4b47b8f7e28f246164953d3fe8b24
Sirojiddin Usmonov <sirojiddinsmnv@gmail.com>
2022-12-01 21:37:48 +0500

    Sending first task

---
+++ net_tasks/DevelopmentAndBuildTools/DevelopmentAndBuildTools/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentAndBuildTools
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some symbols: ");
+            int num1 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The End");
+        }
+    }
+}

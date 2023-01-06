e90c53ef1606d42b7e3a43d91214f488d8d74893
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-01-06 17:14:21 +0500

    Update Program.cs

--- net_tasks/Basics_of_.NET_Framework/Program.cs
+++ net_tasks/Basics_of_.NET_Framework/Program.cs
@@ -3,13 +3,10 @@ using System;
 namespace IntegerInDecimal
 {
     class Program
-    {                                   //Basically, I do not understand the ex., however i used two ways
+    {                                  
         public static void Main()
         {
-            //decimal num = Convert.ToDecimal(Console.ReadLine());        //first way takes int to decimal and shows in the command line
-            //Console.WriteLine($"Your written number is {num}");
-           
-            Console.WriteLine("Write the number with comma. Example, 12,1");        // second way takes decimal and shows in the command line
+            Console.WriteLine("Write the number with comma. Example, 
             float numSecond = float.Parse(Console.ReadLine());
             Console.WriteLine($"Your written decimal number is {numSecond} ");
         }

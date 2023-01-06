c65b5bf070c40fe9990f50668682c2b9b82e98d5
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-01-06 17:15:38 +0500

    Update Program.cs

--- net_tasks/DevelopmentAndBuildTools/Program.cs
+++ net_tasks/DevelopmentAndBuildTools/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevelopmentAndBuildTools
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Enter some symbols: ");
             int num = 0;
@@ -16,5 +12,4 @@ namespace DevelopmentAndBuildTools
             Console.WriteLine($"Your choosen number is {num}");
         }
     }
-
 }

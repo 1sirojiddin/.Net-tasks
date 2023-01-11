d9d99082b808668c0c3bc6f0768e174b0948768a
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-01-11 20:24:51 +0500

    Update Bird.cs

--- net_tasks/Interfaces&AbstractClasses/Bird.cs
+++ net_tasks/Interfaces&AbstractClasses/Bird.cs
@@ -4,13 +4,13 @@ namespace Coordinate
 {
     class Bird : IFlyable              //class Bird
     {
-        public void FlyTo()
+        public void flyTo()
         {
             int speed = 15;                                                  // Bird's speed
             Console.WriteLine($"The bird flight speed is {speed}");
             Console.WriteLine("Press the enter to cross over next chapter");
             Console.ReadLine();
         }
-        public void GetFlyTime(){}
+        public void getFlyTime(){}
     }
 }

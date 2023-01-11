ed02712701a9a2d4ccd602a30b44ae48b7c3076c
Sirojiddin Usmonov <sirojiddinsmnv@gmail.com>
2023-01-11 20:59:12 +0500

    update

--- net_tasks/NetCollections/Truck.cs
+++ net_tasks/NetCollections/Truck.cs
@@ -6,11 +6,11 @@ namespace ProjectCarXmlTransform
     {
         public string modelVehicle { get; set; }
         public int power { get; set; }
-        public string Type { get; set; }
-        public int Volume { get; set; }
-        public int Wheels { get; set; }
-        public string SerialNumber { get; set; }
-        public string Manufacturer { get; set; }
-        public string NumberOfGears { get; set; }
+        public string type { get; set; }
+        public int volume { get; set; }
+        public int wheels { get; set; }
+        public string serialNumber { get; set; }
+        public string manufacturer { get; set; }
+        public string numberOfGears { get; set; }
     }
 }

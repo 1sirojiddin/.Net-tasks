ea62d13bb6fc214ea2e63ce107161412636fbc26
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-01-24 13:39:21 +0500

    Update Union.cs

--- net_tasks/NetCollections/Union.cs
+++ net_tasks/NetCollections/Union.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Exception
+namespace ProjectCarXmlTransform
 {
-    internal class Union
+    class Union
     {
+        public string ModelVehicle { get; set; }
+        public int Power { get; set; }
+        public string Type { get; set; }
+        public int Volume { get; set; }
+        public int Wheels { get; set; }
+        public string SerialNumber { get; set; }
+        public string Manufacturer { get; set; }
+        public string NumberOfGears { get; set; }
     }
 }

49e1aaf899d9c22222c3beb39a3c5da5d52c4800
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-01-24 13:47:23 +0500

    Update Program.cs

--- net_tasks/DevelopmentAndBuildTools/Program.cs
+++ net_tasks/DevelopmentAndBuildTools/Program.cs
@@ -7,14 +7,8 @@ namespace DevelopmentAndBuildTools
         public static void Main()
         {
             Console.WriteLine("Enter some symbols: ");
-<<<<<<< HEAD
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The End");
-=======
-            int num = 0;
-            num = Convert.ToInt32(Console.ReadLine());
+            int num = Convert.ToInt32(Console.ReadLine())
             Console.WriteLine($"Your choosen number is {num}");
->>>>>>> c65b5bf070c40fe9990f50668682c2b9b82e98d5
         }
     }
 }

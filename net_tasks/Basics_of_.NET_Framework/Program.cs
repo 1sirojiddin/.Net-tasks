44a6ddf06085067fc1c97faed435a9b670787a2a
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-01-24 13:43:35 +0500

    Update Program.cs

--- net_tasks/Basics_of_.NET_Framework/Program.cs
+++ net_tasks/Basics_of_.NET_Framework/Program.cs
@@ -9,12 +9,6 @@ namespace IntegerInDecimal
             Console.WriteLine("Write the number with comma. Example,"); 
             float Num = float.Parse(Console.ReadLine());
             Console.WriteLine($"Your written decimal number is {Num} ");
-        {                                  
-        public static void Main()
-        {
-            Console.WriteLine("Write the number with comma. Example, 
-            float numSecond = float.Parse(Console.ReadLine());
-            Console.WriteLine($"Your written decimal number is {numSecond} ");
         }
     }
 }

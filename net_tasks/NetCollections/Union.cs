8f6376168aa6aa3659183090f47587c0166b5561
Sirojiddin Usmonov <sirojiddinsmnv@gmail.com>
2023-02-19 16:08:12 +0500

    updating

--- net_tasks/NetCollections/Union.cs
+++ net_tasks/NetCollections/Union.cs
@@ -4,13 +4,20 @@ namespace ProjectCarXmlTransform
 {
 public class Union
 {
-    public string ModelVehicle { get; set; }
-    public int Power { get; set; }
-    public string Type { get; set; }
-    public int Volume { get; set; }
-    public int Wheels { get; set; }
-    public string SerialNumber { get; set; }
-    public string Manufacturer { get; set; }
-    public string NumberOfGears { get; set; }
-}
+    private string _modelVehicle;
+    private int _power;
+    private string _type;
+    private short _volume;
+    private short _wheels;
+    private string _serialNumber;
+    private string _manufacturer;
+    private string _numberOfGears;
+    public string ModelVehicle { get { return _modelVehicle; } set { _modelVehicle = value; } }
+    public int Power { get { return _power; } set { _power = value; } }
+    public string Type { get { return _type; } set { _type = value; } }
+    public short Volume { get { return _volume; } set { _volume = value; } }
+    public short Wheels { get { return _wheels; } set { _wheels = value; } }
+    public string SerialNumber { get { return _serialNumber; } set { _serialNumber = value; } }
+    public string Manufacturer { get { return _manufacturer; } set { _manufacturer = value; } }
+    public string NumberOfGears { get { return _numberOfGears; } set { _numberOfGears = value; } }  
 }

5acd6d203b6623542bce4349b18224ef4a5d19d5
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-02-19 16:15:49 +0500

    Update Truck.cs

--- net_tasks/OOP/Truck.cs
+++ net_tasks/OOP/Truck.cs
@@ -1,20 +1,20 @@
-﻿using System;
+using System;
 
 namespace CarPark
 {
 public class Truck : CarPark
 {
-    public Truck(short modulPower, byte modulVolume, string modulType, string modulSerialNumber, byte modulWheels, int modulNumberOfSeats, short modulNumberOfGears, string modulManufacturer)
-    {
-        Power = modulPower;
-        Volume = modulVolume;
-        Type = modulType;
-        SerialNumber = modulSerialNumber;
-        Wheels = modulWheels;
-        NumberOfSeats = modulNumberOfSeats;
-        NumberOfGears = modulNumberOfGears;
-        Manufacturer = modulManufacturer;
-    }
-    public override void DefineTo() { }
+public Truck(short modulPower, byte modulVolume, string modulType, string modulSerialNumber, byte modulWheels, int modulNumberOfSeats, short modulNumberOfGears, string modulManufacturer)
+{
+     Power = modulPower;
+     Volume = modulVolume;
+     Type = modulType;
+     SerialNumber = modulSerialNumber;
+     Wheels = modulWheels;
+     NumberOfSeats = modulNumberOfSeats;
+     NumberOfGears = modulNumberOfGears;
+     Manufacturer = modulManufacturer;
+}
+public override void DefineTo() { }
 }
 }

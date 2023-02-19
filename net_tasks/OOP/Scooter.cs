8b785ed4cd501fdd80b87b91635c711eff7ad1a1
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-02-19 16:14:58 +0500

    Update Scooter.cs

--- net_tasks/OOP/Scooter.cs
+++ net_tasks/OOP/Scooter.cs
@@ -1,20 +1,20 @@
-﻿using System;
+using System;
 
 namespace CarPark
 {
 public class Scooter : CarPark
 {
-    public Scooter(short modulPower, byte modulVolume, string modulType, string modulSerialNumber, byte modulWheels, int modulNumberOfSeats, short modulNumberOfGears, string modulManufacturer)
-    {
-        Power = modulPower;
-        Volume = modulVolume;
-        Type = modulType;
-        SerialNumber = modulSerialNumber;
-        Wheels = modulWheels;
-        NumberOfSeats = modulNumberOfSeats;
-        NumberOfGears = modulNumberOfGears;
-        Manufacturer = modulManufacturer;
-    }
-    public override void DefineTo() { }
+public Scooter(short modulPower, byte modulVolume, string modulType, string modulSerialNumber, byte modulWheels, int modulNumberOfSeats, short modulNumberOfGears, string modulManufacturer)
+{
+     Power = modulPower;
+     Volume = modulVolume;
+     Type = modulType;
+     SerialNumber = modulSerialNumber;
+     Wheels = modulWheels;
+     NumberOfSeats = modulNumberOfSeats;
+     NumberOfGears = modulNumberOfGears;
+     Manufacturer = modulManufacturer;
+}
+public override void DefineTo() { }
 }
 }

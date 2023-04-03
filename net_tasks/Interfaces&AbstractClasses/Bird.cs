836b415aa3753dd3f4e0e452cfdaf3c8aa906d9d
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-04-03 11:47:17 +0500

    Update Bird.cs

--- net_tasks/Interfaces&AbstractClasses/Bird.cs
+++ net_tasks/Interfaces&AbstractClasses/Bird.cs
@@ -2,10 +2,10 @@
 
 namespace InterfacesAbstraction
 {
-public class Bird : IFlyable              //class Bird
+public class Bird : IFlyable              
 {
-    private const double maxSpeed = 20;
-    Coordinate CurrentPosition = new Coordinate(4, 5);
+    private const double maxSpeed = 20;  // maximum speed of bird 
+    Coordinate CurrentPosition = new Coordinate(4, 5); // Current position
     public Bird(double speed)
     {     // the bird's speed  will choose randomly
         Random ran = new Random();
